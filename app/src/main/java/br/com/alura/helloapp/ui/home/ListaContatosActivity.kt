4a92805b95679630656e6a1a26420eb82ead002b
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-17 14:28:23 -0300

    Teste de inserção e de execução

--- app/src/main/java/br/com/alura/helloapp/ui/home/ListaContatosActivity.kt
+++ app/src/main/java/br/com/alura/helloapp/ui/home/ListaContatosActivity.kt
@@ -1,17 +1,33 @@
 package br.com.alura.helloapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import br.com.alura.helloapp.database.HelloAppDatabase
+import br.com.alura.helloapp.sampleData.contatosExemplo
 import br.com.alura.helloapp.ui.HelloAppNavHost
 import br.com.alura.helloapp.ui.theme.HelloAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ListaContatosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val contatoDao = HelloAppDatabase.getDatabase(this).contatoDao()
+
+        CoroutineScope(IO).launch {
+//            contatoDao.insere(contatosExemplo.first())
+            val buscaTodos = contatoDao.buscaTodos()
+            Log.i(TAG_LISTA_ACTIVITY, "onCreate: buscando todos os dados")
+            Log.i(TAG_LISTA_ACTIVITY, "onCreate: ${buscaTodos}")
+        }
+
 
         setContent {
             HelloAppTheme {
@@ -21,5 +37,10 @@ class ListaContatosActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    companion object{
+        const val TAG_LISTA_ACTIVITY = "ListaContatosActivity"
+
     }
 }
